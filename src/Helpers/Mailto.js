08c951946535ddf6cd592ca1f620c7ac575c1557
Sharon <ali93456@gmail.com>
2023-05-15 03:11:11 +0300

    Mailto helper explained

--- src/Helpers/Mailto.js
+++ src/Helpers/Mailto.js
@@ -7,3 +7,21 @@ const Mailto = ({ email, subject = "", body = "", children }) => {
 };
 
 export default Mailto;
+
+/*
+Renders a link formatted to send an email (mailto: link).
+
+- Use the email, subject and body props to create a <a> element with an appropriate href attribute.
+- Use encodeURIcomponent to safely encode the subject and body into the link URL.
+- Render the link with children as its content.
+*/
+
+// Usage Example
+
+/*
+
+  <Mailto email="foo@bar.baz" subject="Hello & Welcome" body="Hello world!">
+    Mail me!
+  </Mailto>
+
+*/
