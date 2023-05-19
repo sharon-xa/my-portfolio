64fa047b441e1e77a38c3e48af9cab885a53b215
Sharon <ali93456@gmail.com>
2023-05-19 19:13:05 +0300

    working on making the website responsive

--- src/components/UI/InfoCard.js
+++ src/components/UI/InfoCard.js
@@ -5,11 +5,13 @@ import classes from "./InfoCard.module.css";
 const InfoCard = (props) => {
    const { commandUsed, path } = props;
    return (
-      <div className={classes["card-container"]}>
-         <div className={classes["info-card"]}>
-            <Terminal commandUsed={commandUsed} path={path} />
-            {props.children}
-         </div>
+      <div className={classes["info-card"]}>
+         <Terminal
+            commandUsed={commandUsed}
+            path={path}
+            className={classes.terminal}
+         />
+         {props.children}
       </div>
    );
 };
