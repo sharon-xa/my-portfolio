a048bb22bee89f30059fb0e3949d0d5aad7375cf
Sharon <ali93456@gmail.com>
2023-05-19 20:06:51 +0300

    working on making the project page responsive

--- src/components/Projects/Porjects.js
+++ src/components/Projects/Porjects.js
@@ -5,45 +5,47 @@ import allMyProjects from "./projectsArray";
 const Projects = () => {
    return (
       <main>
-         <section className={classes.container}>
+         <div className={classes.wrapper}>
             <div className={classes.headerBox}>
                <InfoCard commandUsed="ls" path="/home/ali/projects"></InfoCard>
             </div>
-            {allMyProjects.map((project) => {
-               if (!project.image) return null;
-               else
-                  return (
-                     <div className={classes.box} key={project.id}>
-                        <img src={project.image} alt={project.name} />
+            <section className={classes.container}>
+               {allMyProjects.map((project) => {
+                  if (!project.image) return null;
+                  else
+                     return (
+                        <div className={classes.box} key={project.id}>
+                           <img src={project.image} alt={project.name} />
 
-                        <div className={classes.info}>
-                           <h2> {project.name} </h2>
-                           <p>{project.description}</p>
-                        </div>
+                           <div className={classes.info}>
+                              <h2> {project.name} </h2>
+                              <p>{project.description}</p>
+                           </div>
 
-                        <div className={classes.links}>
-                           <a
-                              href={project.demoLink}
-                              target="_blank"
-                              rel="noreferrer"
-                           >
-                              Live Demo
-                           </a>
-                           <a
-                              href={project.codeLink}
-                              target="_blank"
-                              rel="noreferrer"
-                           >
-                              Code
-                           </a>
+                           <div className={classes.links}>
+                              <a
+                                 href={project.demoLink}
+                                 target="_blank"
+                                 rel="noreferrer"
+                              >
+                                 Live Demo
+                              </a>
+                              <a
+                                 href={project.codeLink}
+                                 target="_blank"
+                                 rel="noreferrer"
+                              >
+                                 Code
+                              </a>
+                           </div>
                         </div>
-                     </div>
-                  );
-            })}
+                     );
+               })}
+            </section>
             <div className={classes.footerBox}>
                <InfoCard commandUsed="exit" path=""></InfoCard>
             </div>
-         </section>
+         </div>
       </main>
    );
 };
