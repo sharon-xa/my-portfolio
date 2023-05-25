e5b2076648bace8a414694cddf4f23e355efa8e7
Sharon <ali93456@gmail.com>
2023-05-25 05:16:48 +0300

    bug fixed, when you open the website for the first time you need to click the mode button twice to work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-// import { useState, createContext } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import RootLayout from "./pages/Root";
 import HomePage, { loader } from "./pages/Home";
 import ProjectsPage from "./pages/ProjectsPage";
 import About from "./pages/About";
 import Error from "./pages/Error";
-
-// const ThemeContext = createContext(null);
+import { useEffect } from "react";
 
 function App() {
+   useEffect(() => {
+      localStorage.setItem("selectedTheme", "dark");
+   }, []);
    const router = createBrowserRouter([
       {
          path: "/",
